ece2029b1e6920d50608d8d854ed6c32b09fcba6
silmara <silmaraa.1994@icloud.com>
2023-07-16 15:58:33 -0300

    fm

--- src/components/Atoms/InitialPokemons/index.tsx
+++ src/components/Atoms/InitialPokemons/index.tsx
@@ -11,25 +11,54 @@ const InitialPokemons = () => {
       gap="xxs"
       wrap="wrap"
     >
+
+         <Atom.PokemonGif
+          flip
+          alt="Pokemon pikachu gif"
+          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/25.gif"
+         />
+
+        <Atom.PokemonGif
+        flip
+        alt="Pokemon gengar gif"
+        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/94.gif"
+      />
+
+        <Atom.PokemonGif
+        flip
+        alt="Pokemon squirtle gif"
+        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/7.gif"
+      />
+
+       <Atom.PokemonGif
+        flip
+        alt="Pokemon haunter gif"
+        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/93.gif"
+      />
+
+<Atom.PokemonGif
+        flip
+        alt="Pokemon charmander gif"
+        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/4.gif"
+      />
+
+
+      <Atom.PokemonGif
+        flip
+        alt="Pokemon gastly gif"
+        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/92.gif"
+      />
+
       <Atom.PokemonGif
         flip
         alt="Pokemon bulbasaur gif"
         src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/1.gif"
       />
-      <Atom.PokemonGif
-        flip
-        alt="Pokemon charmander gif"
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/4.gif"
-      />
-      <Atom.PokemonGif
-        flip
-        alt="Pokemon squirtle gif"
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/7.gif"
-      />
-      <Atom.PokemonGif
-        alt="Pokemon pikachu gif"
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/25.gif"
-      />
+
+
+
+
+
     </FlexBox>
   );
 };
